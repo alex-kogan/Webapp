8a5fc8be864aeb03da2ba11bfa3aab7548b62f2c
alex-kogan <alx.kogan@yahoo.com>
2016-01-20 18:22:45 -0500

    Hidden element

--- public/javascripts/appfrontend.js
+++ public/javascripts/appfrontend.js
@@ -1,6 +1,11 @@
 // Front end js
-$(document).ready(function()    {
-        $('.img-container img').click(function()    {
-            window.alert('alex')
-        });
+$(document).ready(function()
+{
+  $('.img-container img').click(function()
+  {
+	  if ( $('#picModal').css('visibility') == 'hidden' )
+	    $('#picModal').css('visibility','visible');
+	  else
+	    $('#picModal').css('visibility','hidden');
+  });  
 });
