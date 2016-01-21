83e91f2379a3a7fa62b9aa9998d3b566a2fb8408
alex-kogan <alx.kogan@yahoo.com>
2016-01-20 23:59:20 -0500

    Added modal support
    Clicking on a picture in the member page opens a modal with a larger
    picture

--- public/javascripts/appfrontend.js
+++ public/javascripts/appfrontend.js
@@ -1,11 +1,8 @@
 // Front end js
 $(document).ready(function()
 {
-  $('.img-container img').click(function()
-  {
-	  if ( $('#picModal').css('visibility') == 'hidden' )
-	    $('#picModal').css('visibility','visible');
-	  else
-	    $('#picModal').css('visibility','hidden');
-  });  
+	$('.img-btn > img').click( function ()
+	{
+		$( "#picButton" ).trigger( "click" );
+	});
 });
